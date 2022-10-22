cc8958541245e4e5edc9131e004e0c76cca6bc53
moji <mojtaba0shahverdi@gmail.com>
2022-10-22 23:53:23 +0330

    Theme

---
+++ config/theme.js
@@ -0,0 +1,50 @@
+const theme = {
+  colors: {
+    primary: '#00cec9',
+    primaryHover: '#00fffb',
+    backgroundPrimary: '#f8f9fa',
+  },
+  typography: {
+    paragraph: {
+      fontSize: 1.266,
+      lineHeight: 1.15,
+      fontWeight: 300,
+    },
+    h1: {
+      fontSize: 1.802,
+      lineHeight: 1.15,
+      fontWeight: 500,
+    },
+    h2: {
+      fontSize: 1.602,
+      lineHeight: 1.15,
+      fontWeight: 500,
+    },
+    h3: {
+      fontSize: 1.424,
+      lineHeight: 1.15,
+      fontWeight: 500,
+    },
+    h4: {
+      fontSize: 1.125,
+      lineHeight: 1.15,
+      fontWeight: 500,
+    },
+    small: {
+      fontSize: 0.889,
+      lineHeight: 1.15,
+      fontWeight: 100,
+    },
+  },
+  spaces: ['0px', '2px', '4px', '8px', '16px', '32px'],
+  borderRadius: ['0px', '2px'],
+  border: ['0px', '2px'],
+  borderColor: {
+    light: '#e3e4e6',
+  },
+  boxShadow: {
+    light: '0 4px 16px rgba(0, 1, 31, 0.2)',
+  },
+}
+
+export default theme
