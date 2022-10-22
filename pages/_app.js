0d63b4bbb4ee2c6ef868d7e29ecef106c0c13e45
moji <mojtaba0shahverdi@gmail.com>
2022-10-22 23:55:19 +0330

    _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import React from 'react'
+import { ThemeProvider } from '@emotion/react'
+import theme from '../config/theme'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// eslint-disable-next-line react/prop-types
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
